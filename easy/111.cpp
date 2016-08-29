ae63fab117873ce05364af7b163301fc353add5d
iceker <iceker@live.com>
2016-08-29 13:48:38 +0800

    爬楼梯

---
+++ easy/111.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+     
+    //¶¯Ì¬¹æ»® 
+    int climbStairs(int n) {
+        // write your code here
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    
+    //ËÑË÷ 
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
+
