1244f35eb74ec41b487e3cd9bb36a568f4478275
iceker <iceker@live.com>
2016-08-29 14:54:58 +0800

    爬楼梯

--- easy/111.cpp
+++ easy/111.cpp
@@ -5,7 +5,7 @@ public:
      * @return: An integer
      */
      
- 
+ 	//动态规划 
     int climbStairs(int n) {
         // write your code here
         vector<int> dp(n+1, 0);
@@ -18,8 +18,8 @@ public:
         return dp[n];
     }
     
-
-    public int climbStairs(int n) {
+	//递归  会超时 
+    int climbStairs(int n) {
         if(n==1) return 1;
         if(n==2) return 2;
         return climbStairs(n-1) + climbStairs(n-2);
